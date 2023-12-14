a6bd4b143b077efd23e2592a70159c6784915ae9
brian yobra <briannjoroge89@gmail.com>
2023-12-14 08:39:48 +0300

    Add files via upload

--- 0x04-TypeScript/task_4/js/subjects/Subject.ts
+++ 0x04-TypeScript/task_4/js/subjects/Subject.ts
@@ -1,11 +1,11 @@
 /// <reference path="Teacher.ts" />
 
 namespace Subjects {
-    export class Subject {
-      teacher: Subjects.Teacher;
-  
-      set setTeacher(teacher: Teacher) {
-        this.teacher = teacher;
-      }
+  export class Subject {
+    teacher: Subjects.Teacher;
+
+    set setTeacher(teacher: Teacher) {
+      this.teacher = teacher;
     }
   }
+}
